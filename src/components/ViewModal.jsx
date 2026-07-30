bd3ef239b627a4a21f74fd0ca6472cf061d5ce4f
rishikagautam-byte <rishika.gautam@ascella.in>
2026-07-30 23:33:21 +0530

    update

--- src/components/ViewModal.jsx
+++ src/components/ViewModal.jsx
@@ -9,14 +9,12 @@ function ViewModal({ note, onClose }) {
           <h2 className="text-2xl font-bold text-white">
             {note.title}
           </h2>
-
           <button
             onClick={onClose}
             className="text-gray-400 hover:text-white"
           >
             ✕
           </button>
-
         </div>
 
         <div className="max-h-[70vh] overflow-y-auto">
@@ -26,9 +24,7 @@ function ViewModal({ note, onClose }) {
           </p>
 
         </div>
-
       </div>
-
     </div>
   );
 }
